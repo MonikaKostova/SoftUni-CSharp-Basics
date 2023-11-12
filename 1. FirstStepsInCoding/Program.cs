5074762c74befbef8ddb321838f820917916b496
MonikaKostova <137040970+MonikaKostova@users.noreply.github.com>
2023-11-12 10:19:33 +0200

    adding

---
+++ 1. FirstStepsInCoding/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace P01.UsdToBgn
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // четем USD от конзолата
+            double usd = double.Parse(Console.ReadLine());
+            //преобразуване в лева
+            double bgn = usd * 1.79549;
+            //otpeqatvane na leva na konzolata
+            Console.WriteLine(bgn);
+        }
+    }
+}
---
+++ 2. RadiansToDegrees/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace P02._RadiansToDegrees
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // четем radiani от конзолата
+            double radians = double.Parse(Console.ReadLine());
+            //преобразуване в gradusi градус = радиан * 180 / π
+            double degrees = radians * 180 / Math.PI;
+            //otpeqatvane na gradusite na konzolata
+            Console.WriteLine(degrees); 
+        }
+    }
+}
